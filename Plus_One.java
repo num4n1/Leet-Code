865f2bf89a863bec68d5ef58d439b4da0163ae27
Mohamed Numan <73037918+num4n1@users.noreply.github.com>
2021-12-22 16:11:08 -0700

    Create Plus_One.java

---
+++ Plus_One.java
@@ -0,0 +1,17 @@
+public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i--)
+        {
+            if(++digits[i] != 10) 
+            {
+                return digits;
+            }
+            else
+            {
+                digits[i] = 0;
+            }
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+        
+    }
