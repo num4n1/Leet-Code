e07084a1f92c0e3f3ed0cea67874ae697c17f801
Mohamed Numan <73037918+num4n1@users.noreply.github.com>
2021-12-23 11:52:43 -0700

    Update Valid_Palindrome.java

--- Valid_Palindrome.java
+++ Valid_Palindrome.java
@@ -1,4 +1,4 @@
-public boolean isPalindrome(String s) {
+    public boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]", "");
         System.out.println(s);
         if(s.length()==0)
@@ -6,9 +6,9 @@ public boolean isPalindrome(String s) {
             return true;
         }
         s=s.toLowerCase();
-        for(int beginIndex = 0; beginIndex < s.length(); beginIndex++)
+        for(int i=0; i<s.length(); i++)
         {
-            if(s.charAt(beginIndex) != s.charAt(s.length()-1-beginIndex)) {
+            if(s.charAt(i) != s.charAt(s.length()-1-i)) {
                 return false;
             }
         }
